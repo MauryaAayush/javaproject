9652b5d96d71cb41a8187ccb93953efd2a31982b
Aayush <maayush235@gmail.com>
2023-09-03 21:52:20 +0530

    1

---
+++ src/Sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the value of a : ");
+        byte a = input.nextByte();
+
+        System.out.print("Enter the value of b : ");
+        byte b = input.nextByte();
+
+        int c = a + b;
+
+        System.out.println("Here is your addition : "+c);
+
+
+    }
+}
