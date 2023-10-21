46e252ce43d00cbd3172c86acd02922c04007467
Aayush <maayush235@gmail.com>
2023-10-21 21:31:09 +0530

    12

--- src/Pattern2/P1.java
+++ src/Pattern2/P1.java
@@ -7,6 +7,14 @@ public class P1 {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
 
 
         
